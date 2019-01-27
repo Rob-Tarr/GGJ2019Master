33e18087fe3bfa0363ce8aa5a94b1ab3043e1009
dcarone5311 <dcarone@uci.edu>
2019-01-26 17:00:16 -0800

    New fresh

---
+++ Assets/Scripts/Fall.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fall : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D Other)
+    {
+        if (Other.name == "Player")
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+}
